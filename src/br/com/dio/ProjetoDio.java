11b4803e18ee1915fff24db13e733beebcc3af8c
Lauro Leal <lauroleal2011@gmail.com>
2021-09-19 14:22:07 -0300

    First Commit

---
+++ src/br/com/dio/ProjetoDio.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class ProjetoDio {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+       /* int a = 3, b = 2;
+
+        System.out.println("hello Word!" + (a+b));*/
+    }
+}
